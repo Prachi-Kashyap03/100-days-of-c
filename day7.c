8e8d1f334329f7540de4aa5c3eeb428c6a79065e
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-10 22:50:48 +0530

    Add number swapping program in day7.c
    Implement a program to swap two numbers without using a third variable.

---
+++ day7.c
@@ -0,0 +1,13 @@
+// swapping no.s without using third variable
+#include<stdio.h>
+int main(){
+  int a, b;
+printf("enter two no.s:");
+scanf("%d%d",&a,&b);
+printf("before swapping:a=%d,b=%d\n",a,b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("after swapping=a=%d,b=%d\n",a,b);
+return 0;
+}
