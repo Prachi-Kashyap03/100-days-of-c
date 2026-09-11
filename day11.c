20487c8af0ed4f6cfe268bfb5c501bba90b35d68
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-11 21:34:39 +0530

    Create day11.c

---
+++ day11.c
@@ -0,0 +1,14 @@
+// find the no. is odd or even using if else
+#include<stdio.h>
+int main(){
+  int a;
+printf("enter the integer:");
+scanf("%d",&a);
+if(a%2==0){
+printf("the integer is even");
+}
+else{
+printf("the given integer is odd");
+}
+return 0;
+}
