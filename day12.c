b8c1f989ba9f2e1d58d553675c8041fe85fd83e5
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-11 21:52:18 +0530

    Implement number sign checker in day12.c
    Add a program to check if a number is positive, negative, or zero.

---
+++ day12.c
@@ -0,0 +1,17 @@
+//check whether the no. is posistive ,negative or zero
+#include<stdio.h>
+int main(){
+  int a;
+printf("enter the number:");
+scanf("%d" ,&a);
+if(a>=0){
+  if(a>0){
+printf("the number is positive");}
+else{
+printf("the number is zero");}
+}
+else{
+printf("the number is negative");
+}
+return 0;
+}
