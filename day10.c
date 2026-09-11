edc318aaf141161c35b5a9bdec769d04b98aa742
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-11 21:21:11 +0530

    Create day10.c

---
+++ day10.c
@@ -0,0 +1,13 @@
+//convert time into hours minutes second format
+#include<stdio.h>
+int main(){
+  int total_seconds,hours,minutes,seconds;
+printf("enter the time in seconds:");
+scanf("%d",&total_seconds);
+hours=total_seconds/3600;
+minutes=(total_seconds % 3600)/60;
+seconds= total_seconds %60;
+printf("time=%d hours %d minutes %d seconds\n",hours,minutes,seconds);
+  return 0;
+}
+  
