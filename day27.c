8fe145967c994d068228ebec7ce8a416f7b49c07
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-14 01:29:55 +0530

    Implement sum of first n odd numbers in day27.c
    Fixes syntax errors and implements the calculation of the sum of the first n odd numbers.

---
+++ day27.c
@@ -0,0 +1,12 @@
+// sum of first n odd numbers
+#include<stdio.h>
+int main(){
+  int n ,i,sum=0;
+printf("enter the value of n:");
+scan("%d",&n);
+for(i=1;i<=n;i++){
+sum=sum+(2*i-1);
+}
+printf("sum of first %d odd numbers =%d",n,sum);
+return 0;
+}
