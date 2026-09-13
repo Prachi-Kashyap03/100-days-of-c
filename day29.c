3200c570411cdfbad672e60388287f929f3880f7
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-14 02:50:14 +0530

    Implement factorial calculation in day29.c

---
+++ day29.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int n, i;                  
+  int factorial = 1;
+    printf("Enter the value of n: ");
+     scanf("%d", &n);
+     for(i = 1; i <= n; i++) {
+      factorial = factorial * i;                                                                                                       } 
+     printf("Factorial of %d = %d\n", n, factorial); 
+       return 0;
+        }
