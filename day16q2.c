983a9399efe6a83c371a2270af356e8567ab4786
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-17 17:14:19 +0530

    Create day16q2.c

---
+++ day16q2.c
@@ -0,0 +1,18 @@
+//palindrome
+#include<stdio.h>
+int main(){
+  int n,original,reverse=0,remainder;
+printf("enter a number:");
+scanf("%d",&n);
+original=n;
+while(n!=0){
+remainder=n%10;
+reverse=reverse*10+remainder;
+n=n/10;
+}
+if(original==reverse)
+  printf("the number is palindrome.");
+else
+  printf("the number is not a palindrome.");
+return 0;
+}
