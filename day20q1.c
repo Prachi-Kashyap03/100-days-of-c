0c8400ae10fd9221759f7424c4c52d90dc720bcf
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-17 22:09:17 +0530

    Create day20q1.c

---
+++ day20q1.c
@@ -0,0 +1,16 @@
+//product of odd digits of a number
+#include<stdio.h>
+int main(){
+  int n,digit,product=1;
+printf("enter a number:");
+scanf("%d",&n);
+while(n!=0){
+digit = n%10;
+if(digit%2!=0){
+product=product*digit;
+}
+n=n/10;
+}
+printf("product of odd digits=%d",product);
+return 0;
+}
