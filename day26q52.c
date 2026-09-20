dc26a8781c99a274244b5595b78874f373c70354
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-20 12:07:52 +0530

    Create day26q52.c

---
+++ day26q52.c
@@ -0,0 +1,53 @@
+/* *
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+*/
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    // 1 star
+    for (i = 1; i <= 1; i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    // 3 stars
+    for (i = 1; i <= 3; i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    // 5 star
+    for (i = 1; i <= 5; i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    // 3 star
+    for (i = 1; i <= 3; i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    // 1 star
+    for (i = 1; i <= 1; i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    return 0;
+}
