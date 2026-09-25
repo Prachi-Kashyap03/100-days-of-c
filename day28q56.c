8ff82efe3bba543213b4c28dfc226eee1d4d43fc
Prachi-Kashyap03 <prachi.38992@stu.upes.ac.in>
2026-09-25 20:06:01 +0530

    Create day28q56.c

---
+++ day28q56.c
@@ -0,0 +1,20 @@
+// print elements of a one-dimensional array
+#include <stdio.h>
+int main()
+{
+    int a[100], n, i;
+    printf("enter no. of elements:");
+    scanf("%d", &n);
+    printf("enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("array elements are:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
